eb34db148d310f14f3de172f2f683447de6b31f3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-03-19 09:08:27 -0500

    Show status of multiple stations

--- check.rb
+++ check.rb
@@ -16,22 +16,29 @@ class Station
     @available_bikes = data["availableBikes"]
     @available_docks = data["availableDocks"]
   end
+
+  def description
+    "#{name} has #{available_bikes} bikes and #{available_docks} open docks"
+  end
 end
 
 stations = {
-  48: Station.new(48, "600w"),
-  74: Station.new(74, "Erie")
+  48 => Station.new(48, "600w"),
+  74 => Station.new(74, "Erie")
 }
 
 while true do
   raw = open("http://www.divvybikes.com/stations/json").read
   json = JSON.parse raw
-  station_data = json["stationBeanList"].each{|station_data|
+  station_data = json["stationBeanList"].each do |station_data|
     station = stations[station_data["id"]]
     next unless station
-    stations.map(&:id).include h["id"] }.each do |data|
-    stations[data["id"]].update_from_feed(data)
+
+    station.update_from_feed(station_data)
   end
-  puts "#{Time.now.strftime("%-I:%M%P")} #{station["stationName"]} has #{station["availableBikes"]} bikes and #{station["availableDocks"]} open slots."
+
+  station_list = stations.sort.map{|s| s[1] }
+  puts "#{Time.now.strftime("%-I:%M%P")} | #{station_list.map(&:description).join(" | ")}"
+
   sleep 60
 end
