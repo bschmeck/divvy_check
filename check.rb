5007d98590a4ccbdcd27d7ee4f59020a9856158e
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-07-16 11:33:16 -0500

    Don't break when the host is unreachable.

--- check.rb
+++ check.rb
@@ -35,7 +35,7 @@ def get_station_data
   raw = open("http://www.divvybikes.com/stations/json").read
   json = JSON.parse raw
   yield json
-rescue SocketError
+rescue SocketError, Errno::EHOSTUNREACH
   nil
 end
 
